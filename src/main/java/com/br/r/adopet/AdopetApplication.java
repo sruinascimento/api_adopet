9e671b03f7c5a9a88d65d9a5dd36758255a944f5
Rui Nascimento <sruy19@gmail.com>
2023-05-26 14:55:21 -0300

    feat: entry point application

---
+++ src/main/java/com/br/r/adopet/AdopetApplication.java
@@ -0,0 +1,13 @@
+package com.br.r.adopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetApplication.class, args);
+	}
+
+}
