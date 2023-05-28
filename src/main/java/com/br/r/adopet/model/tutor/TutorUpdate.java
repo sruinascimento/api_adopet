e73f3b0bd615e0a1439c49367e4a21be94158544
Rui Nascimento <sruy19@gmail.com>
2023-05-28 15:15:26 -0300

    refactor: class name renamed to use with PUT and PATCH http method

--- src/main/java/com/br/r/adopet/model/tutor/TutorUpdate.java
+++ src/main/java/com/br/r/adopet/model/tutor/TutorUpdate.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record TutorUpdatePut(
+public record TutorUpdate(
         @NotNull
         Long id,
         @NotBlank @Pattern(regexp = "[a-zA-Zçãẽĩõũáéíóúâêîôûà]{3,}")
