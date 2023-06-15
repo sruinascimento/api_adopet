fb61242a768b999df7f200f3dee03cefe6288457
Rui Nascimento <sruy19@gmail.com>
2023-06-14 22:41:54 -0300

    refactor: collumn profile_photo added

--- src/main/resources/db/migration/V1__create-table-tutor.sql
+++ src/main/resources/db/migration/V1__create-table-tutor.sql
@@ -2,7 +2,8 @@ CREATE TABLE tutors
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
+    profile_foto TEXT NOT NULL,
     phone_number VARCHAR(13) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    personal_description TEXT
+    personal_description TEXT NOT NULL
 );
